ad869a7701ca9e73011a09617b237e944c81793a
Jaime Bisuna <jaimebisona@gmail.com>
2019-10-05 17:30:09 +0800

    Second Commit

--- app/src/main/java/com/example/mysecondapplication/MainActivity.java
+++ app/src/main/java/com/example/mysecondapplication/MainActivity.java
@@ -3,22 +3,54 @@ package com.example.mysecondapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View body = findViewById(R.id.body);
-        body.setBackgroundColor(170);
+        body.setBackgroundColor(Color.rgb(29, 36, 42));
+        final Switch toggleSwitch = findViewById(R.id.toggleSwitch);
+        toggleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView weightLbl = findViewById(R.id.weightLbl);
+                TextView heightLbl = findViewById(R.id.heightLbl);
+                EditText weightTxt = findViewById(R.id.weight);
+                EditText heightTxt = findViewById(R.id.height);
+                Button calculate = findViewById(R.id.calculate);
+                if(isChecked) {
+                    weightLbl.setText("Weight (lbs)");
+                    heightLbl.setText("Height (in)");
+                    calculate.setText("Imperial");
+                    weightTxt.setHint("Weight (lbs)");
+                    heightTxt.setHint("Height ((ft * 12) + in -> in)");
+                    toggleSwitch.setText("Imperial");
+                    Toast.makeText(getApplicationContext(), "Pounds/in", Toast.LENGTH_LONG).show();
+                } else {
+                    weightLbl.setText("Weight (kg)");
+                    heightLbl.setText("Height (cm)");
+                    calculate.setText("Metric");
+                    weightTxt.setHint("Weight (kg)");
+                    heightTxt.setHint("Height (cm)");
+                    toggleSwitch.setText("Metric");
+                    Toast.makeText(getApplicationContext(), "KG/cm", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void calculate(View view, EditText weightVal, EditText heightVal) {
@@ -37,17 +69,40 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void imperial(View view, EditText weightVal, EditText heightVal) {
+        Intent intent;
+        float weight = Integer.parseInt(weightVal.getText().toString());
+        float height = Integer.parseInt(heightVal.getText().toString());
+        float BMI = (703 * weight) / square(height);
+        // Store data in HashMap with String and String as key Value Pairs
+        Map<String, String> dataMap = new LinkedHashMap<>();
+        dataMap.put("Category", validate(BMI));
+        dataMap.put("Result", Float.toString(Math.round(BMI)));
+        // Attempt to store data in JSON file
+        System.out.println(dataMap.toString());
+        intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("BMI_Output", dataMap.toString());
+        startActivity(intent);
+    }
+
     public void validate(View view) {
         EditText mEdit = findViewById(R.id.weight);
         EditText mEdit2 = findViewById(R.id.height);
-        String weightVal = mEdit.toString();
-        String heightVal = mEdit2.toString();
-        Intent main = new Intent(this, MainActivity.class);
-        if(weightVal.isEmpty() && heightVal.isEmpty()) {
-            startActivity(main);
-            Toast.makeText(this, "Please do not leave fields blank.", Toast.LENGTH_LONG).show();
+        String weightVal = mEdit.getText().toString();
+        String heightVal = mEdit2.getText().toString();
+        Button calculate = findViewById(R.id.calculate);
+        if(calculate.getText().equals("Metric")) {
+            if(weightVal.isEmpty() || heightVal.isEmpty()) {
+                Toast.makeText(this, "Please do not leave fields blank.", Toast.LENGTH_LONG).show();
+            } else {
+                calculate(view, mEdit, mEdit2);
+            }
         } else {
-            calculate(view, mEdit, mEdit2);
+            if(weightVal.isEmpty() || heightVal.isEmpty()) {
+                Toast.makeText(this, "Please do not leave fields blank.", Toast.LENGTH_LONG).show();
+            } else {
+                imperial(view, mEdit, mEdit2);
+            }
         }
     }
 
@@ -59,8 +114,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toggleSomething(View view) {
-        System.out.println("Hello World");
-        Toast.makeText(this, "Event Toggled", Toast.LENGTH_LONG).show();
+        View body = findViewById(R.id.body);
+        int redVal = (int)Math.round(Math.random() * 150);
+        int greenVal = (int)Math.round(Math.random() * 150);
+        int blueVal = (int)Math.round(Math.random() * 150);
+        body.setBackgroundColor(Color.rgb(redVal, greenVal, blueVal));
     }
 
     public String validate(float BMI) {
--- app/src/main/java/com/example/mysecondapplication/ResultActivity.java
+++ app/src/main/java/com/example/mysecondapplication/ResultActivity.java
@@ -3,6 +3,7 @@ package com.example.mysecondapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         TextView output = findViewById(R.id.result);
+        View body = findViewById(R.id.resultPage);
+        body.setBackgroundColor(Color.rgb(29, 36, 42));
         Intent intent = getIntent();
         String BMI_res = intent.getStringExtra("BMI_Output");
         output.setText(BMI_res);
